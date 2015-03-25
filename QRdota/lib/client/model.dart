bef5a6cb300abb28b5cb3e23fe260aa75f0cd747
Urchin <anabelxrita@gmail.com>
2015-03-25 16:50:15 +0800

    fine tune model structure

--- QRdota/lib/client/model.dart
+++ QRdota/lib/client/model.dart
@@ -1,5 +1,10 @@
 library client.model;
 
+import "dart:async";
+
+import "bridge.dart";
+import "../generic/command.dart";
+
 part 'src/model/items.dart';
 part 'src/model/lattice.dart';
 part 'src/model/map_info.dart';
--- QRdota/lib/client/src/model/lattice.dart
+++ QRdota/lib/client/src/model/lattice.dart
@@ -1,1 +1,26 @@
 part of client.model;
+
+class Lattice {
+  String image;
+  String story;
+  num energy;
+  List<String> pos;
+  
+  Future charge(num power) {
+    final List json = _jsonOfCharge(power);
+    return const WriteCommand(SC_USE_SPELL).send(json);
+  }
+  
+  Future update() {
+    final List json = _jsonOfUpdate();
+    return const WriteCommand(SC_USE_SPELL).send(json);
+  }
+  
+  List _jsonOfCharge(num power) {
+    return new List();
+  }
+  
+  List _jsonOfUpdate() {
+    return new List();
+  }
+}
